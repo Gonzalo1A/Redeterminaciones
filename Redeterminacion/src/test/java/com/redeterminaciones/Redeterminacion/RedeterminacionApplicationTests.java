765232464f0d535963e20a228eca2a5250db1a05
Gonzalo <gesellgon137@gmail.com>
2023-12-22 11:35:20 -0300

    Base del proyecto

---
+++ Redeterminacion/src/test/java/com/redeterminaciones/Redeterminacion/RedeterminacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redeterminaciones.Redeterminacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeterminacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
