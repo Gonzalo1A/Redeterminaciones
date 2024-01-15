c81e141769de2bde928de39c778321a2bf74eed1
Octavio Palacios <127260323+OctavioPala@users.noreply.github.com>
2024-01-15 19:33:48 -0300

    150124

--- Redeterminacion/src/main/java/com/redeterminaciones/Redeterminacion/entidades/Empresa.java
+++ Redeterminacion/src/main/java/com/redeterminaciones/Redeterminacion/entidades/Empresa.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Entity
@@ -16,6 +17,7 @@ public class Empresa {
     
     private String nombre;
     private Double oferta;
+    @OneToOne
     private ComputoYPresupuesto computoYPresupuesto;
     
 
