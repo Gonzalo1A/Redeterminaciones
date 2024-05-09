b074013ed6ef2b0807dcf325b520c1db55a26c72
Gonzalo <gesellgon137@gmail.com>
2024-05-09 11:15:44 -0300

    carga de incidencia con input dinamico

--- Redeterminacion/src/main/resources/static/js/vistaObra.js
+++ Redeterminacion/src/main/resources/static/js/vistaObra.js
@@ -1,15 +1,30 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-        // Obtener el botón que abre el modal
-        var modalButton = document.querySelector('[data-bs-target="#modalIncidenciaFactor"]');
-
-        // Agregar un evento click al botón
-        modalButton.addEventListener('click', function (event) {
-            // Obtener el valor de item.id
-            var itemId = event.target.dataset.itemId;
-            console.log('Valor de item.id:', itemId);
-            // Asignar el valor de item.id al campo oculto en el formulario
-            document.getElementById('itemId').value = itemId;
-        });
+$(document).ready(function () {
+    $(".edit-button").click(function () {
+        var inputField = $(this).closest("tr").find(".factor-input");
+        inputField.prop("disabled", !inputField.prop("disabled"));
+        inputField.focus();
     });
-    
+});
+
+function saveFactor(element) {
+    var itemId = $(element).attr('id'); // Obtiene el ID del elemento
+    var factorValue = $(element).val(); // Obtiene el valor del campo de entrada
+
+    // Envía los datos al controlador mediante AJAX
+    $.ajax({
+        url: '/obra/cargarIncidendcia', // Ruta del controlador
+        type: 'POST', // Método HTTP
+        data: {
+            idItem: itemId,
+            incidenciaFactor: factorValue
+        }, // Datos a enviar
+        success: function (response) {
+            // Maneja la respuesta del servidor
+            console.log('Valor de incidencia de factor guardado exitosamente');
+        },
+        error: function (xhr, status, error) {
+            // Maneja los errores
+            console.error('Error al guardar el valor de incidencia de factor:', error);
+        }
+    });
+}
