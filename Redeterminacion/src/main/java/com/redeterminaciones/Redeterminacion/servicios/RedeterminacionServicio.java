56c7039e76fa374c3a2d12a6fa597a64c1523d76
Gonzalo <gesellgon137@gmail.com>
2024-06-19 19:00:14 -0300

    Update RedeterminacionServicio.java

--- Redeterminacion/src/main/java/com/redeterminaciones/Redeterminacion/servicios/RedeterminacionServicio.java
+++ Redeterminacion/src/main/java/com/redeterminaciones/Redeterminacion/servicios/RedeterminacionServicio.java
@@ -1,6 +1,8 @@
 package com.redeterminaciones.Redeterminacion.servicios;
 
+import com.redeterminaciones.Redeterminacion.entidades.Redeterminacion;
 import com.redeterminaciones.Redeterminacion.repositorios.RedeterminacionRepositorio;
+import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,10 @@ import org.springframework.stereotype.Service;
 public class RedeterminacionServicio {
     @Autowired
     private RedeterminacionRepositorio redeterminacionRepositorio;
+    
+    public void crearRedeterminacion(LocalDate mesSolicitud){
+        Redeterminacion redeterminacion = new Redeterminacion();
+        redeterminacion.setMesSolicitud(mesSolicitud);
+        redeterminacionRepositorio.save(redeterminacion);
+    }
 }
