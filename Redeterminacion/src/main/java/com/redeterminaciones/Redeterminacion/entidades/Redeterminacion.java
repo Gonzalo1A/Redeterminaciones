e8d7a53bcbff70b9840f5b87457eb10aa8ea9244
Gonzalo <gesellgon137@gmail.com>
2024-09-02 19:12:37 -0300

    correccion Entidad

--- Redeterminacion/src/main/java/com/redeterminaciones/Redeterminacion/entidades/Redeterminacion.java
+++ Redeterminacion/src/main/java/com/redeterminaciones/Redeterminacion/entidades/Redeterminacion.java
@@ -1,6 +1,9 @@
 package com.redeterminaciones.Redeterminacion.entidades;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,5 +29,6 @@ public class Redeterminacion {
     private LocalDate mesSolictudAnterior;
     private Double variacionReferencia;
     @Lob
+    @Column(columnDefinition="BLOB")
     private byte[] resumenRedet;
 }
