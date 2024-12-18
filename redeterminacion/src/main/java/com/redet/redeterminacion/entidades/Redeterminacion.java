47bf2c9baf4c32fd1a7698c061535e9e2281f6fc
Gonzalo <gesellgon137@gmail.com>
2024-12-18 14:55:43 -0300

    para okkc

--- redeterminacion/src/main/java/com/redet/redeterminacion/entidades/Redeterminacion.java
+++ redeterminacion/src/main/java/com/redet/redeterminacion/entidades/Redeterminacion.java
@@ -1,9 +1,7 @@
 package com.redet.redeterminacion.entidades;
 
-import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,6 +27,6 @@ public class Redeterminacion {
     private LocalDate mesSolictudAnterior;
     private Double variacionReferencia;
     @Lob
-    @Column(columnDefinition="BLOB")
+    @Column(columnDefinition = "BLOB")
     private byte[] resumenRedet;
 }
